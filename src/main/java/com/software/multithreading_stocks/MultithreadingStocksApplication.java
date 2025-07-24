333ef267158e4ad0e04d0d69d0678732da285b1a
Felipe Ribeiro <ribeiro.feu@gmail.com>
2025-07-23 23:48:59 -0300

    project structure created

---
+++ src/main/java/com/software/multithreading_stocks/MultithreadingStocksApplication.java
@@ -0,0 +1,13 @@
+package com.software.multithreading_stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingStocksApplication.class, args);
+	}
+
+}
